a95aca6267f7d6871c1d3414641d9aacfd24cde0
DiegoDiazMtz <diegodiaz220308@gmail.com>
2023-06-27 00:39:48 -0600

    Avance de la interfaz del index

--- YourSpace/app.py
+++ YourSpace/app.py
@@ -6,9 +6,7 @@ app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_DB'] = "yourSpace"
-
 app.secret_key = 'mysecretkey'
-
 mysql = MySQL(app)
 
 #-------------------------------------------------------
@@ -17,42 +15,51 @@ mysql = MySQL(app)
 def login():
     return render_template('login.html')
 
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/ingresar', methods=['POST'])
 def ingresar():
     if request.method == 'POST':
         Vusername = request.form['username']
         Vpassword = request.form['password']
-        Vrol = request.form['rol']
 
     usu = {
         'diego_d': '1234',
-        'maya_g': '4321'
+        'pato_m': '4321'
     }
 
-    if Vusername in usu and usu[Vusername] == Vpassword:
-        session['username'] = Vusername
-        if Vusername == 'diego_d':
-            if Vrol == 'student':
-                return redirect(url_for('indexS'))
-            else: 
-                return redirect(url_for('indexT'))
-        elif Vusername == Vusername:
-            if Vrol == 'student':
-                return redirect(url_for('indexS'))
-            else: 
-                return redirect(url_for('indexT'))
+    if Vusername == 'diego_d':
+        if Vusername in usu and usu[Vusername] == Vpassword:
+            session['username'] = Vusername
+            return redirect(url_for('index'))
+        else: 
+            flash('Usuario o contraseña incorrectos')
+            return redirect(url_for('login'))
+    elif Vusername == 'pato_m':
+        if Vusername in usu and usu[Vusername] == Vpassword:
+            session['username'] = Vusername
+            return redirect(url_for('index'))
+        else: 
+            flash('Usuario o contraseña incorrectos')
+            return redirect(url_for('login'))
     else:
         flash('Usuario o contraseña incorrectos')
+        return redirect(url_for('login'))
+    
 
-    return redirect(url_for('login'))
+@app.route('/cursos')
+def cursos():
+    return render_template('cursos.html')
 
-@app.route('/indexS')
-def indexS():
-    return render_template('indexS.html')
 
-@app.route('/indexT')
-def indexT():
-    return render_template('indexT.html')
+@app.route('/tendencias')
+def tendencias():
+    return render_template('tendencias.html')
+
 
 #-------------------------------------------------------
 
